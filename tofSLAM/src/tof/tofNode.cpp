80ae073802eea9a794fd41f6511e6b47b3603279
Ömür Yıldırım <omuryildirim@ymail.com>
2024-06-07 16:59:12 +0200

    feat: publish messages every second from tof sensor reader to tof topic
    implements #1

--- tofSLAM/src/tof/tofNode.cpp
+++ tofSLAM/src/tof/tofNode.cpp
@@ -4,33 +4,34 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <chrono>
+#include <memory>
+
+using namespace std::chrono_literals;
 using json = nlohmann::json;
 
 
 class TofNode : public rclcpp::Node
 {
 private:
-json messages;
+	json read_json_file(const std::string &file_path)
+	{
+    	std::ifstream file(file_path);
+    	if (!file.is_open()) {
+        	RCLCPP_ERROR(this->get_logger(), "Could not open file: %s", file_path.c_str());
+        	return "";
+    	}
 
-private:
-json read_json_file(const std::string &file_path)
-{
-    std::ifstream file(file_path);
-    if (!file.is_open()) {
-        RCLCPP_ERROR(this->get_logger(), "Could not open file: %s", file_path.c_str());
-        return "";
-    }
-
-    // Parse the JSON file
-    try {
-        std::ifstream f(file_path);
-        json data = json::parse(f);
-        return data;
-    } catch (json::parse_error &e) {
-        RCLCPP_ERROR(this->get_logger(), "JSON parse error: %s", e.what());
-        return "";
-    }
-};
+    	// Parse the JSON file
+    	try {
+    	    std::ifstream f(file_path);
+    	    json data = json::parse(f);
+    	    return data;
+    	} catch (json::parse_error &e) {
+    	    RCLCPP_ERROR(this->get_logger(), "JSON parse error: %s", e.what());
+    	    return "";
+    	}
+	}
 
 public:
     TofNode()
@@ -39,28 +40,38 @@ public:
         RCLCPP_INFO(this->get_logger(), "Hello ROS 2");
         std::string file_path = "/mnt/d/Projects/vecihi/tofSLAM/data/A0_ToF_data_pack.json";  // Update this with the actual file path
         messages = read_json_file(file_path);
+        publisher_ = this->create_publisher<std_msgs::msg::String>("tof", 10);
+    	timer_ = this->create_wall_timer(
+            std::chrono::seconds(1),
+			std::bind(&TofNode::timer_callback, this)
+		);
+		count_ = 0;
     }
 
 private:
-void timer_callback()
-{
-    auto message = std_msgs::msg::String();
-    json ex3 = {
-        {"x", messages["pos"]["x"][0]},
-        {"y", messages["pos"]["y"][0]},
-        {"z", messages["pos"]["z"][0]},
-        {"roll", messages["pos"]["roll"][0]},
-        {"pitch", messages["pos"]["pitch"][0]},
-        {"yaw", messages["pos"]["yaw"][0]},
-        {"tof", messages["tof"]["distances"][0]}
-    };
-    message.data = ex3.dump();
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
-}
+	void timer_callback()
+	{
+    	auto message = std_msgs::msg::String();
+    	json ex3 = {
+    	    {"x", messages["pos"]["x"][count_]},
+    	    {"y", messages["pos"]["y"][count_]},
+        	{"z", messages["pos"]["z"][count_]},
+        	{"roll", messages["pos"]["roll"][count_]},
+        	{"pitch", messages["pos"]["pitch"][count_]},
+        	{"yaw", messages["pos"]["yaw"][count_]},
+        	{"tof", messages["tof"]["distances"][count_]}
+    	};
+    	message.data = ex3.dump();
+    	RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    	publisher_->publish(message);
+	    count_++;
+	}
 
-rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::TimerBase::SharedPtr timer_;
+	rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+	json messages;
+	// define int count_ to keep track of the number of messages published
+    int count_;
 };
 
 int main(int argc, char * argv[])
